fca989d6f0a9198a4bd2d4fb30643d138f45f538
Igor Chernakov <igor.chernakov@gmail.com>
2026-09-18 17:10:02 +0200

    Define M_PI in fill_gap_test for Windows MinGW

--- apps/imageeditor/tests/fill_gap_test.c
+++ apps/imageeditor/tests/fill_gap_test.c
@@ -7,6 +7,11 @@
 #include "test_framework.h"
 #include "test_env.h"
 #include "apps/imageeditor/imageeditor.h"
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 app_state_t *g_app = NULL;
 
